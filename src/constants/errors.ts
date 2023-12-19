34669684df091f7550b3e20a7a69375bd37e4007
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-19 13:49:13 -0300

    feat: add cast/webtoken errors to errors config file

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -1,4 +1,7 @@
 export default {
   UNHANDLED_REJECTION: "unhandledRejection",
   UNCAUGHT_EXCEPTION: "uncaughtException",
+  CAST_ERROR: "CastError",
+  JSON_WEBTOKEN_ERROR: "JsonWebTokenError",
+  TOKEN_EXPIRED_ERROR: "TokenExpiredError",
 };
