0b43d6d4768d423777ca2bf7e53706c4ba21a3b6
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-20 15:52:15 -0300

    feat: add gethistories route

--- src/routers/history.ts
+++ src/routers/history.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { mustAuth, validate } from "../middleware";
-import { updateHistory, removeHistory } from "../controllers";
+import { updateHistory, removeHistory, getHistories } from "../controllers";
 import { UpdateHistorySchema } from "../utils";
 
 const router = Router();
 
 router.post("/", mustAuth, validate(UpdateHistorySchema), updateHistory);
 router.delete("/", mustAuth, removeHistory);
+router.get("/", mustAuth, getHistories);
 
 export { router as historyRouter };
