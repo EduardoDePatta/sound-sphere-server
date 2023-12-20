56a9aa95ff9f6252c9a6fa119d9800a25d441caa
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-20 16:55:52 -0300

    feat: create isauth middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -3,6 +3,7 @@ import catchAsync from "../utils/catchAsync";
 import { PasswordResetToken, User } from "../models";
 import AppError from "../utils/appError";
 import { JwtPayload, verify } from "jsonwebtoken";
+import { formatProfile } from "../utils/helper";
 
 export const isValidPasswordResetToken: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -26,12 +27,14 @@ export const isValidPasswordResetToken: RequestHandler = catchAsync(
   }
 );
 
+const splitTokenFromRequest = (req: Request) => {
+  const { authorization } = req.headers;
+  return authorization?.split("Bearer ")[1];
+};
+
 export const mustAuth: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { authorization } = req.headers;
-
-    const token = authorization?.split("Bearer ")[1];
-
+    const token = splitTokenFromRequest(req);
     if (!token) {
       return next(new AppError("Unauthorized request!", 403));
     }
@@ -45,15 +48,29 @@ export const mustAuth: RequestHandler = catchAsync(
       return next(new AppError("Unauthorized request!", 403));
     }
 
-    req.user = {
-      id: user._id,
-      name: user.name,
-      email: user.email,
-      verified: user.verified,
-      avatar: user.avatar?.url,
-      followers: user.followers.length,
-      followings: user.followings.length,
-    };
+    req.user = formatProfile(user);
+
+    next();
+  }
+);
+
+export const isAuth: RequestHandler = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const token = splitTokenFromRequest(req);
+
+    if (token) {
+      const payload = verify(token, process.env.JWT_SECRET ?? "") as JwtPayload;
+      const id = payload.userId;
+
+      const user = await User.findOne({ _id: id, tokens: token });
+
+      if (!user) {
+        return next(new AppError("Unauthorized request!", 403));
+      }
+
+      req.user = formatProfile(user);
+      req.token = token;
+    }
 
     next();
   }
